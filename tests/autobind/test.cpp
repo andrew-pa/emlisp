1a988bb71ff3e4170e1a8caa364f55719b63d075
Andrew Palmer <andrew.pa@outlook.com>
2023-01-24 11:34:07 -0700

    fix tests

--- tests/autobind/test.cpp
+++ tests/autobind/test.cpp
@@ -48,8 +48,8 @@ int main(int argc, char* argv[]) {
         std::cout << "testfn\n";
         c.reset();
         rt.define_global("f", rt.make_extern_reference(&f));
-        {rt.eval(rt.read("(test-fn/times 10 (lambda (i) (counter/increment c i)))"));
-            assert(c.value == 55);}
+        {rt.eval(rt.read("(test-fn/times f 10 (lambda (i) (counter/increment c i)))"));
+            assert(c.value == 45);}
     } catch(emlisp::type_mismatch_error e) {
         std::cout << "error: " << e.what() << "; actual = " << e.actual << ", expected = " << e.expected << "\n";
         exit(3);
